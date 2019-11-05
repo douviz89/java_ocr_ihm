bdca869574bbbb27067bc610ffcca7a913927404
Douviz <Douviz@192.168.0.11>
2019-11-05 20:23:23 +0100

    graphic chapter

--- src/ocr_partie2_ihm/Fenetre.java
+++ src/ocr_partie2_ihm/Fenetre.java
@@ -1,30 +1,19 @@
 package ocr_partie2_ihm;
 
 import java.awt.HeadlessException;
-
 import javax.swing.JFrame;
 
 public class Fenetre extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 	public Fenetre() throws HeadlessException {
-		// Définit un titre pour notre fenêtre
 		this.setTitle("My first java window");
-			
-		// Définit sa taille : 400 pixels de large et 100 pixels de haut
-		this.setSize(400, 500);
-		
+		this.setSize(400, 400);
 		// Nous demandons maintenant à notre objet de se positionner au centre
 		this.setLocationRelativeTo(null);
-		
-		// Termine le processus lorsqu'on clique sur la croix rouge
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		// Et enfin, la rendre visible
+
+		this.setContentPane(new Panneau());
+
 		this.setVisible(true);
 	}
 
--- src/ocr_partie2_ihm/Test.java
+++ src/ocr_partie2_ihm/Test.java
@@ -1,10 +1,9 @@
 package ocr_partie2_ihm;
 
-import javax.swing.JFrame;
-
 public class Test {
 	public static void main(String[] args) {
 
-		Fenetre f = new Fenetre();
+		Fenetre fen = new Fenetre();
+		
 	}
 }
